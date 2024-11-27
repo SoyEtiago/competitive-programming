968bec16ec005d584fb75ecf1e4d6ac8e329e69c
Etienne Samboni <etiennesamboni@gmail.com>
2024-11-27 11:28:51 -0500

    Done Number Spiral excercise

--- cses/Introductory Problems/Number Spiral.cpp
+++ cses/Introductory Problems/Number Spiral.cpp
@@ -9,13 +9,13 @@ int main() {
  
   while(ntest--) {
     long long x = 0LL, y = 0LL;
-    cin >> x >> y;
+    cin >> y >> x;
      long long max_num = max(x, y); 
     // Manhattan distance d1(p,q) = |p1-q1| + |p2-q2|
-    // Even max_num => cord : (max_num, 1)
-    // Odd max_num => cord : (1, max_num)
-    if(max_num % 2 == 0) cout << (max_num*max_num) - (abs(x-max_num) + abs(y-1LL)) << "\n";
-    else cout << (max_num*max_num) - (abs(x-1LL) + abs(y-max_num)) << "\n";
+    // Even max_num => cartesian cord : (1, max_num)
+    // Odd max_num => cartesian cord : (max_num, 1) 
+    if(max_num % 2 == 0) cout << (max_num*max_num) - (abs(x-1LL) + abs(y-max_num)) << "\n";
+    else cout << (max_num*max_num) - (abs(x-max_num) + abs(y-1LL)) << "\n";
   }
  
   return 0;
